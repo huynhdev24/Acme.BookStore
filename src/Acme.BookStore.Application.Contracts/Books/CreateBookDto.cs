5c094f03e2fc24f123c403cc029a66eafee0f5ad
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-05 15:13:08 +0700

    update BookDto and add CreateBookDto

---
+++ src/Acme.BookStore.Application.Contracts/Books/CreateBookDto.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+using Volo.Abp.Application.Dtos;
+
+namespace Acme.BookStore.Books
+{
+    public class CreateBookDto : AuditedEntityDto<Guid>
+    {
+        [Required]
+        [StringLength(128)]
+        public string Name { get; set; }
+
+        [Required]
+        public BookType Type { get; set; } = BookType.Underfined;
+
+        [Required]
+        [DataType(DataType.Date)]
+        public DateTime PublishDate { get; set; } = DateTime.Now;
+
+        [Required]
+        public float Price { get; set; }
+    }
+}
