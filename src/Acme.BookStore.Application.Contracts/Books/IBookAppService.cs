8607aea6778286a1693e6822dc0a114d0664034e
huynhdev24 <huynh.dev.24@gmail.com>
2023-05-05 15:44:02 +0700

    Implement AuthorAppService

--- src/Acme.BookStore.Application.Contracts/Books/IBookAppService.cs
+++ src/Acme.BookStore.Application.Contracts/Books/IBookAppService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Volo.Abp.Application.Dtos;
 using Volo.Abp.Application.Services;
 
@@ -13,6 +14,7 @@ namespace Acme.BookStore.Books
             PagedAndSortedResultRequestDto,
             CreateUpdateBookDto>
     {
-
+        Task<ListResultDto<AuthorLookupDto>> GetAuthorLookupAsync();
+        Task<AuthorWithDetailsDto> CreateAuthorWithBooksAsync(CreateAuthorWithBookDto input);
     }
 }
--- src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissionDefinitionProvider.cs
+++ src/Acme.BookStore.Application.Contracts/Permissions/BookStorePermissionDefinitionProvider.cs
@@ -8,9 +8,27 @@ public class BookStorePermissionDefinitionProvider : PermissionDefinitionProvide
 {
     public override void Define(IPermissionDefinitionContext context)
     {
-        var myGroup = context.AddGroup(BookStorePermissions.GroupName);
+        var bookStoreGroup = context.AddGroup(BookStorePermissions.GroupName, L("Permission:AngularMaterial"));
+
+        var booksPermission = bookStoreGroup.AddPermission(BookStorePermissions.Books.Default, L("Permission:Books"));
+        booksPermission.AddChild(BookStorePermissions.Books.Create, L("Permission:Books.Create"));
+        booksPermission.AddChild(BookStorePermissions.Books.Edit, L("Permission:Books.Edit"));
+        booksPermission.AddChild(BookStorePermissions.Books.Delete, L("Permission:Books.Delete"));
+
+        var authorsPermission = bookStoreGroup.AddPermission(
+            BookStorePermissions.Authors.Default, L("Permission:Authors"));
+
+        authorsPermission.AddChild(
+            BookStorePermissions.Authors.Create, L("Permission:Authors.Create"));
+
+        authorsPermission.AddChild(
+            BookStorePermissions.Authors.Edit, L("Permission:Authors.Edit"));
+
+        authorsPermission.AddChild(
+            BookStorePermissions.Authors.Delete, L("Permission:Authors.Delete"));
+
         //Define your own permissions here. Example:
-        //myGroup.AddPermission(BookStorePermissions.MyPermission1, L("Permission:MyPermission1"));
+        //myGroup.AddPermission(AngularMaterialPermissions.MyPermission1, L("Permission:MyPermission1"));
     }
 
     private static LocalizableString L(string name)
